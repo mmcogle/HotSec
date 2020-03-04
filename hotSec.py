4debedb345aa3dc9613606904cb823eae0a46f20
Cameron Ogle <mmcogle@gmail.com>
2020-03-04 21:11:48 +0000

    updates

--- hotSec.py
+++ hotSec.py
@@ -3,7 +3,7 @@ import subprocess
 
 
 def get_tweets():
-    url = "https://api.twitter.com/1.1/search/tweets.json?q=%23infosec&result_type=popular"
+    url = "https://api.twitter.com/1.1/search/tweets.json?q=%23infosec&result_type=popular&tweet_mode=extended"
     headers = {
             'Authorization':'Bearer AAAAAAAAAAAAAAAAAAAAALy8CAEAAAAAIQXreUYSiLupPYKU5mljoIneJM0%3Dx491a9pXymvlNzbjjSuXB2kaH8VUmEYywLURsDiCQzyiksmnSt',
             'user-agent':'advanced-rest-client',
@@ -11,8 +11,11 @@ def get_tweets():
             }
     response = requests.get(url, headers=headers)
     response = response.json()
-    print(response['statuses'][0]['text'])
-    print("ugh")
+    for resp in response['statuses']:
+        print("\n------------------------------------------")
+        print (resp['full_text'])
+    #print(response['statuses'][0]['text'])
+    #print("ugh")
 
 if __name__=='__main__':
     get_tweets()
